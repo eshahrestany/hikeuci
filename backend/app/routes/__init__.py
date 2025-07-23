68c343bf5a7cbe2860a4efdf8a2d057b41de57de
Gabe <gabriel@gdodge.dev>
2025-07-23 15:32:14 -0700

    minor linter changes

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -2,6 +2,7 @@ from .auth import auth
 from .dashboard import dashboard
 from .images import images
 
+
 def register_routes(app):
     app.register_blueprint(auth, url_prefix="/api/auth")
     app.register_blueprint(dashboard, url_prefix="/api/dashboard")
