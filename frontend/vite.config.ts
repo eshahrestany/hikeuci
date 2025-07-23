05ea00929c89f4ff19db9d346791e4fe5acf26d6
Gabe <gabriel@gdodge.dev>
2025-07-23 15:31:54 -0700

    Decouple frontend from backend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,4 +14,13 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    port: 5001,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:5000',
+        changeOrigin: true
+      }
+    }
+  }
 })
