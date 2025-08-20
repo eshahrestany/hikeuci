6a5d72ca5a845dad9c4f55432adf65a3a08168e9
evan <evan@eshahrestany.dev>
2025-08-20 03:35:59 +0000

    initialize celery for async email tasks

--- backend/app/extensions.py
+++ backend/app/extensions.py
@@ -1,5 +1,18 @@
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from celery import Celery, Task
+
+def celery_init_app(app):
+    class FlaskTask(Task):
+        def __call__(self, *args: object, **kwargs: object):
+            with app.app_context():
+                return self.run(*args, **kwargs)
+
+    celery_app = Celery(app.name, task_cls=FlaskTask)
+    celery_app.config_from_object(app.config["CELERY"])
+    celery_app.set_default()
+    app.extensions["celery"] = celery_app
+    return celery_app
 
 db = SQLAlchemy()
 migrate = Migrate()
